6d6c88192ad73ad367b9bb15d5a31d499d60b632
rolex <drolewski@gmail.com>
2020-06-03 23:16:49 +0200

    Update root app and files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {DataService} from './functional/data/data.service';
+import {FormsModule} from '@angular/forms';
+import {FunctionalModule} from './functional/functional.module';
+import {PagesModule} from './pages/pages.module';
+import {RoutingModule} from './functional/routing/routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    FunctionalModule,
+    RoutingModule,
+    PagesModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
